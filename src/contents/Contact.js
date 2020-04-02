c69d8234ae8fe6d6ebdb3030e90287714b93e4b6
arifannyrs <arifannyrs@gmail.com>
2020-04-02 19:46:37 +0700

    membuat Contact

---
+++ src/contents/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Social from '../components/Social';
+class Contact extends Component {
+    render() {
+        return (
+            <div className="condiv">
+            <h1 className="subtopic">Kontak Saya</h1>
+            <h3>Email  :   arifannyrs@gmail.com</h3>
+            <h3>Instagram   :   @arifannyrs</h3>
+            <h3>Whatsapp   :   +6281615657604</h3>
+            <Social />
+            </div>
+            )
+        }
+    }
+    
+    export default Contact
