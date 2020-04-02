19712b30102156ddf63aded7eba0d3ede6e17962
arifannyrs <arifannyrs@gmail.com>
2020-04-02 19:45:34 +0700

    membuat About

---
+++ src/contents/About.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class About extends Component {
+    render() {
+        return (
+            <div className="condiv">
+            <h1 className="subtopic">Tentang Saya</h1>
+            <h4>Halo,</h4>
+            <h1>Saya, Arifanny Ramadhan Sukma</h1>
+            <h3>Mahasiswa Poltikenik Negeri Malang  | <u>Teknik Informatika</u></h3>
+            <br></br>
+            <p>Saya sedang mengerjakan UTS Pemrograman Berbasis Framework
+            </p>
+            
+            </div>
+            )
+        }
+    }
+    
+export default About
